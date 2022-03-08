cde8d90fd910cd8f2ece2a272ca89ad07ed8c4c2
benkabongo25 <kabongo.ben025@gmail.com>
2022-03-08 15:57:23 +0100

    user ban - add end time
    Signed-off-by: benkabongo25 <kabongo.ben025@gmail.com>

--- ducky/src/model/objects/users/UserBan.js
+++ ducky/src/model/objects/users/UserBan.js
@@ -3,7 +3,7 @@ export class UserBan {
     constructor(userId, time, endTime, reason) {
         this.userId = userId;
         this.time = time;
-        this.endTime = time;
+        this.endTime = endTime;
         this.reason = reason;
     }
 }
