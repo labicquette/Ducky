f2242bfaf889ff1535b10f482a6766d96e475bed
benkabongo25 <kabongo.ben025@gmail.com>
2022-03-16 02:36:45 +0100

    components sign in sign up

--- ducky/src/view/components/Logo.js
+++ ducky/src/view/components/Logo.js
@@ -3,12 +3,10 @@ import React from 'react';
 export default class Logo extends React.Component {
     render() {
         return (
-            
             <img 
                 src={require('../../ressources/ducky.png')} 
                 alt='Ducky' 
                 title='Ducky'
-                style={{'width': '5%'}}
                 />
         );
     }
