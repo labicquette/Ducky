1de2054180ba69a24ae152f8afb10784d4a1a88b
benkabongo25 <kabongo.ben025@gmail.com>
2022-04-10 15:51:00 +0200

    add update profils pages

---
+++ ducky/src/view/components/users/UserUpdateStatusView.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class UserUpdateStatusView extends React.Component {
+
+    render() {
+        return (
+            <div className='user-update-profil-view-container'>
+                <div className='user-update-profil-view-title'>
+                    <h3>Status du compte</h3>
+                </div>
+                <div className='user-update-profil-view-content'>
+                    
+                </div>
+            </div>
+        );
+    }
+}
