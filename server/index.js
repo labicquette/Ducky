af7ba215a3af840c9e0e6e606969de90cd799d88
Théo <komete17@gmail.com>
2022-04-14 19:43:18 +0200

    added session management but not tested

--- server/index.js
+++ server/index.js
@@ -14,12 +14,18 @@ const login = require("./routes/login");
 const Users = require("./entities/users");
 //const Login = require("./entities/login");
 
+const session = require("express-session");
+
+
 const db = {} 
 db.users = new DataStore({filename: '../database/dataUsers.txt', autoload: true});
 db.users.ensureIndex({fieldName: 'username',fieldName: 'mail',fieldName:'phone', unique: true})
 
 
 db.posts = new DataStore({filename: '../database/dataPosts.txt', autoload: true});
+
+db.login = new DataStore({filename: '../database/sessions.txt', autoload:true});
+db.login.ensureIndex({fieldName: 'session_id', unique: true, expireAfterSeconds: 2628000})
 
 dbInterface = {}
 dbInterface.users = new Users(db)
@@ -30,7 +36,11 @@ app.use(express.json());
     // simple logger for this router's requests
     // all requests to this router will first hit this middleware
 
-app.use(session)
+app.use(session({
+    secret: "chaine <<aleatoire>>",
+    cookie: {maxAge : 30000},
+    saveUninitialized: false
+}));
 
 app.use((req, res, next) => {
   console.log('API: method %s, path %s', req.method, req.path);
@@ -38,10 +48,11 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/", login(db.login));
 
 //app.use("/posts", posts(dbInterface));
 app.use("/users", users(dbInterface));
-//app.use("/login", login(dbInterface));
+
 
 
 
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,25 +1,45 @@
-const express = require("express")
+const express = require("express");
+const session = require("express-session");
+
+function initRouter(db){
+
 let router = express.Router();
 
-router.get("", (req, res) => {
-    res.send("cookie");
+
+
+router.get("login", (req, res) => {
+    
+    req.session.regenerate(function(err){
+        if(err){
+            res.status(500).send("error at setting cookies");
+        }
+        else{
+            db.insert({session_id: req.sessionID}, function(err, newSessionId){
+                if(err){
+                    res.status(500).send(err);
+                }else{
+                    res.session = req.session;
+                    res.send("connected");
+                }
+            })
+        }
+    })
+    res.send("logged in");
 });
 
-router.post("", (req,res)  => {
-    res.send("new post");
-});
+router.all("",(req,res,next)=>{
+    if(req.session.session_id){
+        if(db.find({session_id : req.session.session_id},function(err, docs){
+            if(docs){return docs}
+            else{return false}
+        })){
+            next();
+        }else{
+            res.status(403).send("Session Timed Out")
+        }
+    }
+})
 
-router.delete("/:postid", (req, res) => {
-    res.send("post deleted");
-});
-
-router.get("/:postid", (req, res) =>{
-    res.send("get 1 post");
-});
-
-router.patch("/:postid", (req, res)=>{
-    //requete bd patch 
-    res.send("patched post");
-});
-
-module.exports = router 
+return router
+}
+module.exports = initRouter 
